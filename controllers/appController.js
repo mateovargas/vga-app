0d816ade7f408537d154d11824e583f79b613340
Mateo Vargas <mateovargas5@gmail.com>
2018-03-17 13:12:48 -0700

    Adding routes to hit api for franchises, characters, and platfors.

--- controllers/appController.js
+++ controllers/appController.js
@@ -43,17 +43,70 @@ router.get("/api/:game", function(req, res){
         ]).then(function (result) {
             console.log(result);
 
-            var hbsObject = {
-                game: result.body[0]
-            };
-
-            console.log(hbsObject);
-
             res.json(result.body[0]);
         });
 });
 
-router.get("/:id", function(req, res){
+router.get("/api/:franchise", function(req, res){
+
+    igdbClient.franchise({
+        search: createSearchTerm(req.params.franchise),
+        limit: 1
+    },
+    [
+        'name',
+        'games'
+    ]).then(function(result){
+        console.log(result);
+        res.json(result.body[0]);
+    })
+});
+
+router.get("/api/:character", function(req, res){
+    igdbClient.character({
+        search: createSearchTerm(req.params.character),
+        limit: 1
+    },
+    [
+        'name',
+        'akas',
+        'species',
+        'games'
+    ]).then(function(result){
+        res.json(result.body[0]);
+    });
+});
+
+router.get("/api/:platform", function(req,res){
+    igdbClient.platform({
+        search: createSearchTerm(req.params.platform),
+        limit: 5
+    },
+    [
+        'name',
+        'logo',
+        'summary',
+        'generation',
+    ]).then(function(result){
+        res.json(result.body);
+    });
+});
+
+router.get("/api/:id", function(req, res){
+    igdbClient.external_review_source({
+        ids: [
+            req.params.id
+        ],
+        limit: 10
+        },
+        [
+            'url',
+        ]).then(function(res){
+            res.json(result.body);
+        });
+});
+
+router.get("/games/:id", function(req, res){
 
     igdbClient.games({
         ids: [
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -19,21 +19,54 @@ $(document).ready(function(){
         return url;
     }
 
+    function searchGame(searchTerm) {
+
+        var gameURL = createURL(searchTerm);
+
+        $.ajax({
+            method: "GET",
+            url: "/api/" + gameURL
+        }).then(function (res) {
+
+            window.location = "/games/" + res.id;
+
+        });
+
+    }
+
+    function searchFranchise(searchTerm){
+
+        var franchiseURL = createURL(searchTerm);
+
+        $.ajax({
+            method: "GET",
+            url: "/api/" + franchiseURL
+        }).then(function(res){
+            window.location = "/franchise/" + res.id
+        });
+    }
+
+    function searchCharacter(searchTerm) {
+
+        var characterURL = createURL(searchTerm);
+
+        $.ajax({
+            method: "GET",
+            url: "/api/" + characterURL
+        }).then(function (res) {
+            window.location = "/character/" + res.id
+        });
+    }
+
 
     $("#index-submit").on("submit", function(event){
         
         event.preventDefault();
 
         var searchTerm = $("#index-input").val().trim();
-        var gameURL = createURL(searchTerm);
 
-        $.ajax({
-            method: "GET",
-            url: "/api/" + gameURL
-        }).then(function(res){
+        searchGame(searchTerm);
 
-            window.location = "/" + res.id;
-            
-        });
     });
+
 })
